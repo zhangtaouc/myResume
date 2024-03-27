62c42b23d589b1240566d91a52786b818614e34b
20023638 <zhangtao.xd@haier.com>
2024-03-27 16:05:59 +0800

    新配置

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,28 +1,28 @@
 import { httpGet, httpPost, httpDelete } from './request'
 import { UserListResponse, User } from '@/map/interface'
 
+const apiUrl = 'http://1.94.101.51:8081'
+
 // 查询全部用户
 export function getUserListApi(): Promise<UserListResponse> {
-  return httpGet('http://localhost:8081/student/getAll')
+  return httpGet(`${apiUrl}/student/getAll`)
 }
 
 export function getUserByNameApi(name: string): Promise<UserListResponse> {
-  return httpGet('http://localhost:8081/student/getStudentByName?name=' + name)
+  return httpGet(`${apiUrl}/student/getStudentByName?name=${name}`)
 }
 
 // 添加用户
 export function addUserApi(body: User): Promise<UserListResponse> {
-  return httpPost('http://localhost:8081/student/addStudent', body)
+  return httpPost(`${apiUrl}/student/addStudent`, body)
 }
 
 // 编辑用户
 export function editUserApi(body: User): Promise<UserListResponse> {
-  return httpPost('http://localhost:8081/student/updateStudent', body)
+  return httpPost(`${apiUrl}/student/updateStudent`, body)
 }
 
 // 删除用户
 export function deleteUserApi(username: string): Promise<UserListResponse> {
-  return httpDelete(
-    'http://localhost:8081/student/deleteStudent?name=' + username
-  )
+  return httpDelete(`${apiUrl}/student/deleteStudent?name= ${username}`)
 }
