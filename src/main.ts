7e3fce8c530a698414a565ece64e4ee719cbf855
zhangtaouc <39691213+zhangtaouc@users.noreply.github.com>
2024-04-09 22:26:59 +0800

    使用组件

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import './assets/base.less'
 import 'element-plus/dist/index.css'
+import ZTUI from 'zt-vue3-component/zt-ui/zt-ui.umd.js'
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
+app.use(ZTUI)
 app.mount('#app')
